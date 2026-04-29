3fb3572f35fb29a0fd5c263e78783bc1b20a59ea
Scandevistan <legendaryred369@gmail.com>
2026-04-29 23:41:48 +0530

    Configure Astro for GitHub Pages hosting (Static Site Generation)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wetpets.in',
-  adapter: cloudflare(),
   integrations: [sitemap()],
   image: {
     domains: ['cdn.sanity.io', 'images.unsplash.com']
