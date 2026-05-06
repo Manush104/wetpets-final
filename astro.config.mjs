47d0b89cb326bfbad4dbd4db8c1547a9c150a6b4
Scandevistan <legendaryred369@gmail.com>
2026-05-07 00:48:58 +0530

    chore: configure cloudflare worker adapter and wrangler settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Manush104.github.io',
   base: '/wetpets-final',
   integrations: [sitemap()],
+  output: 'server',
+  adapter: cloudflare(),
   image: {
     domains: ['cdn.sanity.io', 'images.unsplash.com']
   }
